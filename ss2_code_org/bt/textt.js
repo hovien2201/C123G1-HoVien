3555d1527e8adbecf031776d6e073a22aedc7699
hovien2201 <hovien30122017@gmail.com>
2023-02-22 17:10:19 +0700

    ss2_code_org

--- ss2_code_org/bt/textt.js
+++ ss2_code_org/bt/textt.js
@@ -1,4 +1,18 @@
-let productsList=[""];
-function showproducts(){
-    let 
+let productsList=["sony","oppo","iphone"];
+function showProducts(){
+    let conten="";
+    for(let i=0;i<productsList.length;i++){
+        conten =conten+'<tr>'
+        +'<td>'+productsList[i]+'</td>'
+        +'<td>sua</td>'
+        +'<td>xoa</td>'
+        +'</tr>';
+    }
+    document.getElementById("products").innerHTML = conten;
+}
+showProducts();
+function addProducts(){
+    let newP =document.getElementById("newP").value;
+    productsList.push(newP);
+    showProducts();
 }
